229739e3869a942dca6f59cfac1669b48069cbce
Xtertm3 <xtertm3@users.noreply.github.com>
2026-09-24 11:50:02 +0000

    Bump build marker after moving Sling Bags/Bottles/Laptop Bags under Gift Packs

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Footer.css';
 import { STORE_CONTACT } from '../../config/contact';
 
-const BUILD_ID = '2026-09-24-nav-v3';
+const BUILD_ID = '2026-09-24-nav-v4';
 
 export const Footer: React.FC = () => {
   return (
